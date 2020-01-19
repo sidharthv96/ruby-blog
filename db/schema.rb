6844718aecb8ae31804ba309681feb2a9c1cbb47
sidv <sidharthv96@gmail.com>
2020-01-19 11:49:38 +0530

    Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,10 +12,13 @@
 
 ActiveRecord::Schema.define(version: 2020_01_18_184829) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
     t.text "body"
-    t.integer "post_id", null: false
+    t.bigint "post_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
